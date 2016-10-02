257a497a1742cae4a4cef1f16f32eb5ad917b0cc
Andrew Joia <drewdrewthis@gmail.com>
2016-10-02 14:22:11 -0400

    Changed carousel to load all images

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,196 +3,205 @@
 var max_results = 5;
 
 function getData() {
-	return $.ajax({
-		dataType: "json",
-		url: 'https://www.deskbookers.com/nl-nl/sajax.json?q=Amsterdam&type=-&people=any&favorite=0&pid=&sw=52.293753%2C4.634942&ne=52.455562%2C5.162286&ids=17201%2C19640%2C13692%2C13691%2C12136%2C17938%2C15292%2C14886%2C14885%2C14884%2C14883%2C15730%2C15353%2C15351%2C15330%2C15080%2C17290%2C15454%2C15451%2C15379'
-	});
+    return $.ajax({
+        dataType: "json",
+        url: 'https://www.deskbookers.com/nl-nl/sajax.json?q=Amsterdam&type=-&people=any&favorite=0&pid=&sw=52.293753%2C4.634942&ne=52.455562%2C5.162286&ids=17201%2C19640%2C13692%2C13691%2C12136%2C17938%2C15292%2C14886%2C14885%2C14884%2C14883%2C15730%2C15353%2C15351%2C15330%2C15080%2C17290%2C15454%2C15451%2C15379'
+    });
 }
 
 function createCarousel(element, image_arr) {
 
-	var $carousel = $(element);
-	$carousel.current = 0; // 
-	$carousel.images = image_arr;
+    var $carousel = $(element);
+    $carousel.current = 0; // 
+    $carousel.images = image_arr;
 
-	function createControls() {
-		var template = '\n\t\t\t<div class="control-wrapper left">\n\t\t\t\t<div class="control left-control">\n\t\t\t\t\t<i class="fa fa-chevron-left" aria-hidden="true"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<div class="control-wrapper right">\n\t\t\t\t<div class="control right-control">\n\t\t\t\t\t<i class="fa fa-chevron-right" aria-hidden="true"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t';
+    function createControls() {
+        var template = '\n\t\t\t<div class="control-wrapper left">\n\t\t\t\t<div class="control left-control">\n\t\t\t\t\t<i class="fa fa-chevron-left" aria-hidden="true"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<div class="control-wrapper right">\n\t\t\t\t<div class="control right-control">\n\t\t\t\t\t<i class="fa fa-chevron-right" aria-hidden="true"></i>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t';
 
-		$carousel.append(template);
+        $carousel.append(template);
 
-		$carousel.find('.right-control').on('click', function () {
-			$carousel.nextImage();
-		});
+        $carousel.find('.right-control').on('click', function () {
+            $carousel.nextImage();
+        });
 
-		$carousel.find('.left-control').on('click', function () {
-			$carousel.prevImage();
-		});
-	}
+        $carousel.find('.left-control').on('click', function () {
+            $carousel.prevImage();
+        });
+    }
 
-	$carousel.goToImage = function (index) {
-		console.log('Go to image', index);
-		$('.carousel-item').fadeOut();
-		$('.carousel-item[data-id="' + index + '"]').fadeIn();
+    $carousel.goToImage = function (index) {
+        console.log('Go to image', index);
+        $('.carousel-item').fadeOut();
+        $('.carousel-item[data-id="' + index + '"]').fadeIn();
 
-		if ($carousel.images.length === 0) {
-			$carousel.find('.control').hide();
-		} else if ($carousel.current === 0) {
-			$carousel.find('.left-control').hide();
-			$carousel.find('.right-control').show();
-		} else if ($carousel.current === $carousel.images.length - 1) {
-			$carousel.find('.left-control').show();
-			$carousel.find('.right-control').hide();
-		} else {
-			console.log('show both');
-			$carousel.find('.control').show();
-		}
-	};
+        if ($carousel.images.length === 0) {
+            $carousel.find('.control').hide();
+        } else if ($carousel.current === 0) {
+            $carousel.find('.left-control').hide();
+            $carousel.find('.right-control').show();
+        } else if ($carousel.current === $carousel.images.length - 1) {
+            $carousel.find('.left-control').show();
+            $carousel.find('.right-control').hide();
+        } else {
+            console.log('show both');
+            $carousel.find('.control').show();
+        }
+    };
 
-	$carousel.updateImages = function (images) {
-		$carousel.images = images;
-		$carousel.find('ul').html("");
-		images.map(function (url, index) {
-			$carousel.find('ul').append('\n\t\t\t\t\t<li id="image-' + index + '" \n\t\t\t\t\t\tdata-id="' + index + '" \n\t\t\t\t\t\tclass="carousel-item" \n\t\t\t\t\t\tstyle="background-image:url(\'' + url + '\')"></li>\n\t\t\t\t');
-		});
-		$carousel.goToImage(0);
-	};
+    $carousel.updateImages = function (images) {
+        $carousel.images = images;
+        $carousel.find('ul').html("");
+        images.forEach(function (url, index) {
+            if (typeof url === 'string') {
+                // Type check
+                $carousel.find('ul').append('\n\t\t\t\t\t\t<li id="image-' + index + '" \n\t\t\t\t\t\t\tdata-id="' + index + '" \n\t\t\t\t\t\t\tclass="carousel-item" \n\t\t\t\t\t\t\tstyle="background-image:url(\'' + url + '\')"></li>\n\t\t\t\t\t');
+            }
+        });
 
-	$carousel.nextImage = function () {
-		console.log('Go to image');
-		if ($carousel.images[$carousel.current + 1]) {
-			$carousel.goToImage($carousel.current++);
-		}
-	};
+        $carousel.goToImage(0);
+    };
 
-	$carousel.prevImage = function () {
-		if ($carousel.images[$carousel.current - 1]) {
-			$carousel.goToImage($carousel.current--);
-		}
-	};
+    $carousel.nextImage = function () {
+        console.log('Go to image');
+        if ($carousel.images[$carousel.current + 1]) {
+            $carousel.goToImage($carousel.current++);
+        }
+    };
 
-	createControls();
-	$carousel.append('<ul></ul>');
-	$carousel.updateImages(image_arr);
+    $carousel.prevImage = function () {
+        if ($carousel.images[$carousel.current - 1]) {
+            $carousel.goToImage($carousel.current--);
+        }
+    };
 
-	return $carousel;
+    createControls();
+    $carousel.append('<ul></ul>');
+    $carousel.updateImages(image_arr);
+
+    return $carousel;
 }
 
 function predictSearch(str) {
-	var locations = app.model.rows;
-	locations.sort(function (a, b) {
-		return a.name.localeCompare(b.name);
-	});
-	var results = locations.filter(function (place) {
-		return place.name.toLowerCase().includes(str.toLowerCase());
-	});
-	fillPredictor('.predictive-results', results.slice(0, 5));
+    var locations = app.model.rows;
+    locations.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+    });
+    var results = locations.filter(function (place) {
+        return place.name.toLowerCase().includes(str.toLowerCase());
+    });
+    fillPredictor('.predictive-results', results.slice(0, 5));
 }
 
 function fillPredictor(element, arr) {
-	var $predictor = $(element);
-	$predictor.html("");
-	console.log(arr);
-	var _iteratorNormalCompletion = true;
-	var _didIteratorError = false;
-	var _iteratorError = undefined;
+    var $predictor = $(element);
+    $predictor.html("");
+    console.log(arr);
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-	try {
-		for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-			var location = _step.value;
+    try {
+        for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var location = _step.value;
 
-			$predictor.append('\n\t\t\t<li data-id="' + location.location_id + '" \n\t\t\t\tclass="predictive-item"\n\t\t\t\tdata-name="' + location.name + '">' + location.name + '</li>');
-		}
-	} catch (err) {
-		_didIteratorError = true;
-		_iteratorError = err;
-	} finally {
-		try {
-			if (!_iteratorNormalCompletion && _iterator.return) {
-				_iterator.return();
-			}
-		} finally {
-			if (_didIteratorError) {
-				throw _iteratorError;
-			}
-		}
-	}
+            $predictor.append('\n\t\t\t<li data-id="' + location.location_id + '" \n\t\t\t\tclass="predictive-item"\n\t\t\t\tdata-name="' + location.name + '">' + location.name + '</li>');
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
 }
 
 function processSearch(str) {
-	console.log(str);
-	$('#location-search-form input[type="text"]').val("");
+    console.log(str);
+    $('#location-search-form input[type="text"]').val("");
 }
 
 function updateResults(locations) {
-	locations.map(function (location, index) {
-		var workplace = location.name;
-		var location_name = location.location_name;
-		var review_score = location.rating || "-";
-		var price = location.day_price ? '$' + location.day_price + "/day" : "";
-		var image_url = location.image_urls[0];
+    locations.map(function (location, index) {
+        var workplace = location.name;
+        var location_name = location.location_name;
+        var review_score = location.rating || "-";
+        var price = location.day_price ? '$' + location.day_price + "/day" : "";
+        var image_url = location.image_urls[0];
 
-		$('.results-list').append('<li \n        \tid="location-' + index + '" \n        \tclass="result-item" \n        \tstyle="background-image:url(\'' + image_url + '\')">\n\t\t\t\t<ul class="location-info">\n\t\t\t\t\t<li>' + workplace + '</li>\n\t\t\t\t\t<li>' + location_name + '</li>\n\t\t\t\t\t<li>Score: ' + review_score + '</li>\n\t\t\t\t\t<li>' + price + '</li>\n\t\t\t\t</ul>\n\t\t\t</li>');
-	});
+        $('.results-list').append('<li \n        \tid="location-' + index + '" \n        \tclass="result-item" \n        \tstyle="background-image:url(\'' + image_url + '\')">\n\t\t\t\t<ul class="location-info">\n\t\t\t\t\t<li>' + workplace + '</li>\n\t\t\t\t\t<li>' + location_name + '</li>\n\t\t\t\t\t<li>Score: ' + review_score + '</li>\n\t\t\t\t\t<li>' + price + '</li>\n\t\t\t\t</ul>\n\t\t\t</li>');
+    });
 }
 
 function setEventHandlers() {
-	// Search form
-	$('#location-search-form').submit(function (e) {
-		var search_inquiry = $('input[type="text"]').val();
-		e.preventDefault();
-		console.log(search_inquiry);
-		processSearch(search_inquiry);
-		$('input[type="text"]').val("");
-	});
+    // Search form
+    $('#location-search-form').submit(function (e) {
+        var search_inquiry = $('input[type="text"]').val();
+        e.preventDefault();
+        console.log(search_inquiry);
+        processSearch(search_inquiry);
+        $('input[type="text"]').val("");
+    });
 
-	// Search form predictive search
-	$('#location-search-form input[type="text"]').focus(function () {
-		$('#location-search-form input[type="text"]').keydown(function (e) {
-			var search_inquiry = $('#location-search-form input[type="text"]').val();
-			console.log(search_inquiry);
-			predictSearch(search_inquiry);
-			$('.predictive-box').show();
-		});
-	});
+    // Search form predictive search
+    $('#location-search-form input[type="text"]').focus(function () {
+        $('#location-search-form input[type="text"]').keydown(function (e) {
+            var search_inquiry = $('#location-search-form input[type="text"]').val();
+            console.log(search_inquiry);
+            predictSearch(search_inquiry);
+            $('.predictive-box').show();
+        });
+    });
 
-	$('#location-search-form input[type="text"]').focus(function () {
-		$('#location-search-form input[type="text"]').keydown(function (e) {
-			var search_inquiry = $('#location-search-form input[type="text"]').val();
-			console.log(search_inquiry);
-			predictSearch(search_inquiry);
-			$('.predictive-box').show();
-		});
-	});
+    $('#location-search-form input[type="text"]').focus(function () {
+        $('#location-search-form input[type="text"]').keydown(function (e) {
+            var search_inquiry = $('#location-search-form input[type="text"]').val();
+            console.log(search_inquiry);
+            predictSearch(search_inquiry);
+            $('.predictive-box').show();
+        });
+    });
 
-	$('#location-search-form input[type="text"]').focusout(function () {
-		$('.predictive-box').hide();
-	});
+    $('#location-search-form input[type="text"]').focusout(function () {
+        $('.predictive-box').hide();
+    });
 
-	$('.predictive-item').on('click', function () {
-		$('.predictive-results').html("");
-		processSearch($(this).data('name'));
-		$('.predictive-box').hide();
-	});
+    $('.predictive-item').on('click', function () {
+        $('.predictive-results').html("");
+        processSearch($(this).data('name'));
+        $('.predictive-box').hide();
+    });
 }
 
 var app = {
-	init: function init() {
-		$.when(getData()).then(function (data) {
-			console.log('Retrieved data'.data);
-			app.model = data;
-			var locations = data.rows.slice(0, max_results); // Show 
-			var main_images = locations[0].image_urls.concat(locations[0].image_urls2);
+    init: function init() {
+        $.when(getData()).then(function (data) {
+            console.log('Retrieved data'.data);
+            app.model = data;
+            var locations = data.rows.slice(0, max_results); // Show 
+            // This was initially set up to just have a carousel for one location
+            // let main_images = locations[0].image_urls.concat(locations[0].image_urls2);
 
-			// Create components
-			app.main_carousel = createCarousel('carousel', main_images);
-			updateResults(locations);
-		});
+            var main_images = data.rows.reduce(function (all_images, location) {
+                return all_images.concat(location.image_urls);
+            }, []);
 
-		setEventHandlers();
-	}
+            console.log(main_images[0].length);
+            app.main_carousel = createCarousel('carousel', main_images);
+            updateResults(locations);
+        });
+
+        setEventHandlers();
+    }
 
 };
 
 $(document).ready(function () {
-	app.init();
+    app.init();
 });
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,12 +11,12 @@ function getData() {
 
 function createCarousel(element, image_arr) {
 
-	var $carousel = $(element);
-	$carousel.current = 0; // 
-	$carousel.images = image_arr;
-	
-	function createControls() {
-		var template = `
+    var $carousel = $(element);
+    $carousel.current = 0; // 
+    $carousel.images = image_arr;
+
+    function createControls() {
+        var template = `
 			<div class="control-wrapper left">
 				<div class="control left-control">
 					<i class="fa fa-chevron-left" aria-hidden="true"></i>
@@ -29,98 +29,98 @@ function createCarousel(element, image_arr) {
 			</div>
 		`;
 
-		$carousel.append(template);
+        $carousel.append(template);
 
-		$carousel.find('.right-control').on('click', function() {
-			$carousel.nextImage();
-		});
-
-		$carousel.find('.left-control').on('click', function() {
-			$carousel.prevImage();
-		});
-	}
-
-	$carousel.goToImage = function(index) {
-		console.log('Go to image', index);
-		$('.carousel-item').fadeOut();
-		$(`.carousel-item[data-id="${index}"]`).fadeIn();
-
-		if($carousel.images.length === 0 ) {
-			$carousel.find('.control').hide();
-		}
-		else if($carousel.current === 0) {
-			$carousel.find('.left-control').hide();
-			$carousel.find('.right-control').show();
-		} 
-		else if ($carousel.current === $carousel.images.length - 1) {
-			$carousel.find('.left-control').show();
-			$carousel.find('.right-control').hide();
-		}
-		else {
-			console.log('show both');
-			$carousel.find('.control').show();
-		}
-	}
-
-	$carousel.updateImages = function(images) {
-		$carousel.images = images;
-		$carousel.find('ul').html("");
-        images.map(function(url, index) {
-            $carousel.find('ul').append(`
-					<li id="image-${index}" 
-						data-id="${index}" 
-						class="carousel-item" 
-						style="background-image:url('${url}')"></li>
-				`);
+        $carousel.find('.right-control').on('click', function() {
+            $carousel.nextImage();
         });
+
+        $carousel.find('.left-control').on('click', function() {
+            $carousel.prevImage();
+        });
+    }
+
+    $carousel.goToImage = function(index) {
+        console.log('Go to image', index);
+        $('.carousel-item').fadeOut();
+        $(`.carousel-item[data-id="${index}"]`).fadeIn();
+
+        if ($carousel.images.length === 0) {
+            $carousel.find('.control').hide();
+        } else if ($carousel.current === 0) {
+            $carousel.find('.left-control').hide();
+            $carousel.find('.right-control').show();
+        } else if ($carousel.current === $carousel.images.length - 1) {
+            $carousel.find('.left-control').show();
+            $carousel.find('.right-control').hide();
+        } else {
+            console.log('show both');
+            $carousel.find('.control').show();
+        }
+    }
+
+    $carousel.updateImages = function(images) {
+        $carousel.images = images;
+        $carousel.find('ul').html("");
+        images.forEach(function(url, index) {
+            if (typeof url === 'string') { // Type check
+                $carousel.find('ul').append(`
+						<li id="image-${index}" 
+							data-id="${index}" 
+							class="carousel-item" 
+							style="background-image:url('${url}')"></li>
+					`);
+            }
+
+        });
+
         $carousel.goToImage(0);
-	}
+    }
 
-	$carousel.nextImage = function() {
-		console.log('Go to image');
-		if ($carousel.images[$carousel.current + 1]) {
-			$carousel.goToImage($carousel.current++);
-		}	
-	}
+    $carousel.nextImage = function() {
+        console.log('Go to image');
+        if ($carousel.images[$carousel.current + 1]) {
+            $carousel.goToImage($carousel.current++);
+        }
+    }
 
-	$carousel.prevImage = function() {
-		if ($carousel.images[$carousel.current - 1]) {
-			$carousel.goToImage($carousel.current--);
-		}
-	}
+    $carousel.prevImage = function() {
+        if ($carousel.images[$carousel.current - 1]) {
+            $carousel.goToImage($carousel.current--);
+        }
+    }
 
-	createControls();
-	$carousel.append('<ul></ul>');
-	$carousel.updateImages(image_arr)
+    createControls();
+    $carousel.append('<ul></ul>');
+    $carousel.updateImages(image_arr)
 
-	return $carousel;
+    return $carousel;
 }
 
 function predictSearch(str) {
-	let locations = app.model.rows;
-	locations.sort((a,b) => a.name.localeCompare(b.name));
-	let results = locations.filter(function(place) {
-		return place.name.toLowerCase().includes(str.toLowerCase());
-	});
-	fillPredictor('.predictive-results', results.slice(0,5));
+    let locations = app.model.rows;
+    locations.sort((a, b) => a.name.localeCompare(b.name));
+    let results = locations.filter(function(place) {
+        return place.name.toLowerCase().includes(str.toLowerCase());
+    });
+    fillPredictor('.predictive-results', results.slice(0, 5));
 }
 
 function fillPredictor(element, arr) {
-	var $predictor = $(element);
-	$predictor.html("");
-	console.log(arr);
-	for(let location of arr) {
-		$predictor.append(`
+    var $predictor = $(element);
+    $predictor.html("");
+    console.log(arr);
+    for (let location of arr) {
+        $predictor.append(`
 			<li data-id="${location.location_id}" 
 				class="predictive-item"
-				data-name="${location.name}">${location.name}</li>`
-			);
-	}
+				data-name="${location.name}">${location.name}</li>`);
+    }
 }
 
 function processSearch(str) {
-	console.log(str);
-	$('#location-search-form input[type="text"]').val("");
+    console.log(str);
+    $('#location-search-form input[type="text"]').val("");
 }
 
 function updateResults(locations) {
@@ -146,61 +146,66 @@ function updateResults(locations) {
 }
 
 function setEventHandlers() {
-	// Search form
-	$('#location-search-form').submit(function(e) {
-	    let search_inquiry = $('input[type="text"]').val();
-	    e.preventDefault();
-	    console.log(search_inquiry);
-	    processSearch(search_inquiry);
-	    $('input[type="text"]').val("");
-	});
+    // Search form
+    $('#location-search-form').submit(function(e) {
+        let search_inquiry = $('input[type="text"]').val();
+        e.preventDefault();
+        console.log(search_inquiry);
+        processSearch(search_inquiry);
+        $('input[type="text"]').val("");
+    });
 
-	// Search form predictive search
-	$('#location-search-form input[type="text"]').focus(function() {
-		$('#location-search-form input[type="text"]').keydown(function(e) {
-			let search_inquiry = $('#location-search-form input[type="text"]').val();
-			 console.log(search_inquiry);
-			predictSearch(search_inquiry);
-			$('.predictive-box').show();
-		})
-	});
+    // Search form predictive search
+    $('#location-search-form input[type="text"]').focus(function() {
+        $('#location-search-form input[type="text"]').keydown(function(e) {
+            let search_inquiry = $('#location-search-form input[type="text"]').val();
+            console.log(search_inquiry);
+            predictSearch(search_inquiry);
+            $('.predictive-box').show();
+        })
+    });
 
-	$('#location-search-form input[type="text"]').focus(function() {
-		$('#location-search-form input[type="text"]').keydown(function(e) {
-			let search_inquiry = $('#location-search-form input[type="text"]').val();
-			 console.log(search_inquiry);
-			predictSearch(search_inquiry);
-			$('.predictive-box').show();
-		})
-	});
+    $('#location-search-form input[type="text"]').focus(function() {
+        $('#location-search-form input[type="text"]').keydown(function(e) {
+            let search_inquiry = $('#location-search-form input[type="text"]').val();
+            console.log(search_inquiry);
+            predictSearch(search_inquiry);
+            $('.predictive-box').show();
+        })
+    });
 
-	$('#location-search-form input[type="text"]').focusout(function() {
-		$('.predictive-box').hide();
-	});
+    $('#location-search-form input[type="text"]').focusout(function() {
+        $('.predictive-box').hide();
+    });
 
-	$('.predictive-item').on('click', function() {
-		$('.predictive-results').html("");
-		processSearch($(this).data('name'));
-		$('.predictive-box').hide();
-	})	
+    $('.predictive-item').on('click', function() {
+        $('.predictive-results').html("");
+        processSearch($(this).data('name'));
+        $('.predictive-box').hide();
+    })
 }
 
 var app = {
     init: function() {
         $.when(getData()).then(function(data) {
-        	console.log('Retrieved data'. data);
-        	app.model = data;
-        	let locations = data.rows.slice(0, max_results); // Show 
-        	let main_images = locations[0].image_urls.concat(locations[0].image_urls2);
-            
-            // Create components
-            app.main_carousel = createCarousel('carousel',main_images);
-        	updateResults(locations);
+            console.log('Retrieved data'.data);
+            app.model = data;
+            let locations = data.rows.slice(0, max_results); // Show 
+            // This was initially set up to just have a carousel for one location
+            // let main_images = locations[0].image_urls.concat(locations[0].image_urls2);
+
+            let main_images = data.rows.reduce(function(all_images, location) {
+            	return all_images.concat(location.image_urls);
+            }, []);
+
+            console.log(main_images[0].length);
+            app.main_carousel = createCarousel('carousel', main_images);
+            updateResults(locations);
         });
 
         setEventHandlers();
     },
-    
+
 }
 
 $(document).ready(function() {
